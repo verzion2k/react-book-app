cb2fac7c6cb17f9dd3c8372dd3f6cdcf327ce67d
verzion2k <kovacs.zsoltzx@gmail.com>
2019-03-11 10:29:12 +0100

    updated layout

--- src/create_account/CreateAccountHeader.js
+++ src/create_account/CreateAccountHeader.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './sass/CreateAccountHeader.scss';
-import CreateAccountCircle from './CreateAccountCircle';
+import CirclePortal from './CirclePortal';
 
 class CreateAccountHeader extends Component {
 	render() {
 		return (
 			<div className="new__account__header">
-				<CreateAccountCircle />
+				<CirclePortal>
+					<div className="new__account__circle--xs" />
+					<div className="new__account__circle--s" />
+					<div className="new__account__circle--lg" />
+				</CirclePortal>
 				<h2 className="new__account__header--title">Welcome!</h2>
 				<h3 className="new__account__header--desc">
 					Create your account to get started. After that, you can share books and make friends.
