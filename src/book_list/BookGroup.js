cdd829334d27115c9077be9855eca394fbc562fe
verzion2k <kovacs.zsoltzx@gmail.com>
2019-03-12 15:52:10 +0100

    scroll effect added

--- src/book_list/BookGroup.js
+++ src/book_list/BookGroup.js
@@ -10,18 +10,37 @@ import papillon from './icons/papillon-book.svg';
 export default class BookGroup extends Component {
 	render() {
 		return (
-			<div className="book__group">
-				<h2 className="book__group__title">Books Reviewed By You</h2>
+			<div className="book__wrapper">
+				<div className="book__group">
+					<h2 className="book__group__title">Books Reviewed By You</h2>
 
-				<BookItem img={remoteBook} title="Remote: Office Not Required" author="Jason Fried" route="remote" />
-				<BookItem img={papillon} title="Papillon" author="Henri Charriere" route="papillon" />
-				<BookItem
-					img={darkTower}
-					title="The Dark Tower: The Gunslinger"
-					author="Stephen King"
-					route="the-dark-tower"
-				/>
-
+					<BookItem
+						img={remoteBook}
+						title="Remote: Office Not Required"
+						author="Jason Fried"
+						route="remote"
+					/>
+					<BookItem img={papillon} title="Papillon" author="Henri Charriere" route="papillon" />
+					<BookItem
+						img={darkTower}
+						title="The Dark Tower: The Gunslinger"
+						author="Stephen King"
+						route="the-dark-tower"
+					/>
+					<BookItem
+						img={remoteBook}
+						title="Remote: Office Not Required"
+						author="Jason Fried"
+						route="remote"
+					/>
+					<BookItem img={papillon} title="Papillon" author="Henri Charriere" route="papillon" />
+					<BookItem
+						img={darkTower}
+						title="The Dark Tower: The Gunslinger"
+						author="Stephen King"
+						route="the-dark-tower"
+					/>
+				</div>
 				<Button buttonName="Add book review" buttonClassName="book__button" />
 
 				<Portal>
